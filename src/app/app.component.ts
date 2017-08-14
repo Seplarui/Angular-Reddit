3ded9ee9e2e7c116462e0d04f0020fb234460791
Sebas <seplarui@gmail.com>
2017-08-14 23:32:15 +0200

    Multiples Articulos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 ﻿import { Component } from '@angular/core';
+import { Article } from './article/article.model'; //<-- import this
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,18 @@
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'app works!';
+
+    articles: Article[]; //<-- component property
+
+    constructor() {
+        this.articles = [
+            new Article('Angular 2', 'http://angular.io', 3),
+            new Article('Fullstack', 'http://fullstack.io', 2),
+            new Article('Angular Homepage', 'http://angular.io', 1)
+        ];
+    }
+
+
 
     addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
         console.log(`Adding article title: ${title.value} and link: ${link.value}`);
