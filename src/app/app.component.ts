ffff017e651081d3a5a64d624fa9d4f7146ff476
Sebas <seplarui@gmail.com>
2017-08-18 18:47:50 +0200

    Sorted Elementos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,7 +27,7 @@ export class AppComponent {
     }
 
     sortedArticles(): Article[] {
-      return this.articles.sort((a: Article, b:Article) => b.votes -a.votes);
+      return this.articles.sort((a: Article, b:Article) => b.votes - a.votes);
     }
 
 
